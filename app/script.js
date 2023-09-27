bd9035bf703d693122212bda9695c4b212cbecb2
kabipriya-mediwave <kabipriya@mindwaveventures.com>
2023-09-27 15:40:52 +0530

    added validation

--- app/script.js
+++ app/script.js
@@ -1,33 +1,28 @@
 $(document).ready(function () {
   const brr = [];
-  const specialCharacterRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/;
+  const emailRegex = /^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/;
+  const passwordRegex =
+    /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
   btnSubmit = $("#submit");
   btnSubmit.click(function (e) {
     e.preventDefault();
     const email = $("#username").val();
     const password = $("#password").val();
-    const item = {
-      id: new Date().getTime(),
-      email: email,
-      password: password,
-    };
-    if (email) {
-      if (password.length >= 8) {
-        if (specialCharacterRegex.test(password)) {
-          const profile = {
-            email: email,
-            password: password,
-          };
-          brr.push(profile);
-          console.log(JSON.stringify(brr));
-        } else {
-          alert("Password must contain at least one special character");
-        }
+    if (emailRegex.test(email)) {
+      if (passwordRegex.test(password)) {
+        const profile = {
+          email: email,
+          password: password,
+        };
+        brr.push(profile);
+        console.log(JSON.stringify(brr));
       } else {
-        alert(" password should be atleast 8 character");
+        alert(
+          "Password must contain at least 8 characters, including at least one letter, one digit, and one special character."
+        );
       }
     } else {
-      alert("enter valid email");
+      alert("Enter a valid email address.");
     }
   });
 });
